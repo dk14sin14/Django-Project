1a08c7c5a3b9c83480ec8761db68dcc643049fb6
shihuai <tanshihuai@gmail.com>
2022-05-08 02:26:40 +0800

    created location table, linked location to meetups via a foreign key

--- django_course_site/meetups/admin.py
+++ django_course_site/meetups/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Meetup, Location
 
 # Register your models here.
 
 class MeetupAdmin(admin.ModelAdmin):
 	list_display = ('title', 'slug', 'description', 'image')
-	list_filter = ('title', )
+	list_filter = ('title', 'location')
 	prepopulated_fields = {'slug': ('title', )}
 
-admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Meetup, MeetupAdmin)admin.site.register(Location)
