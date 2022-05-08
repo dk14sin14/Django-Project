defab8437e14eed14284d64226f07df4d16c2602
shihuai <tanshihuai@gmail.com>
2022-05-09 03:23:06 +0800

    implemented registration success

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -7,7 +7,9 @@ Determines which view requests should be executed when request reach those URLs.
 '''
 
 
+# IMPT! DYNAMIC PATH SEGMENTS should be below static paths, otherwise django will treat the link as a dynamic path
 urlpatterns = [
     path('meetups/', views.index, name="all-meetups"),
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name="meetup-detail")
+    path('meetups/success', views.confirm_registration, name="confirm_registration"),
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name="meetup-details")
 ]
--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Meetup
 from .forms import RegistrationForm
 
@@ -14,14 +14,25 @@ def index(request):
 
 def meetup_details(request, meetup_slug):
     try:
-        selected_meetup = Meetup.objects.get(slug=meetup_slug)
-        registration_form = RegistrationForm()
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)      # selected_meetup is a Meetup object. get() gets the correct object based on the slug passed in.
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+            
+        else:
+            registration_form = RegistrationForm(request.POST)      # passes in the POST request's fields into RegistrationForm
+            if registration_form.is_valid():                        # checks if inputs are valid, built in function
+                participant = registration_form.save()
+                selected_meetup.participants.add(participant)       # adds the participant created above into the participant attribute in the Meetup object. 
+                return redirect('confirm_registration')
+
         return render(request, 'meetups/meetup-details.html', {
-            'meetup_found': True,
-            'meetup': selected_meetup,
-            'form': registration_form
-        })
+                'meetup_found': True,
+                'meetup': selected_meetup,
+                'form': registration_form
+            })
     except Exception as exc:
         return render(request, 'meetups/meetup-details.html', {'meetup_found': False})
         
 
+def confirm_registration(request):
+    return render(request, 'meetups/registration-success.html')
